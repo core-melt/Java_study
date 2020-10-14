c89343462868509418b4cc90fca7a1b566d7e60d
core-melt <core.melt21@gmail.com>
2020-10-14 16:48:51 +0900

    [Create]file

---
+++ src/JavaFile/FileControl.java
@@ -0,0 +1,11 @@
+package JavaFile;
+
+import java.io.File;
+
+public class FileControl {
+    public static void main(String[] args) {
+    	File file = new File("D:\\自己学習\\java_dwc\\Java_study\\src\\JavaFile", "abc.txt");
+    	System.out.println(file.toString());
+
+    }
+}
